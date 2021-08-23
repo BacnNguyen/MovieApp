25169533b9b03548234608de4d2a168a1aeddd39
LamDev99 <lamitptit99@gmail.com>
2021-08-23 18:37:36 +0700

    complete search using microphone and add Timber to project :necktie:

--- app/src/main/java/gst/trainingcourse/movie/MainApplication.kt
+++ app/src/main/java/gst/trainingcourse/movie/MainApplication.kt
@@ -2,8 +2,14 @@ package gst.trainingcourse.movie
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MainApplication: Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
