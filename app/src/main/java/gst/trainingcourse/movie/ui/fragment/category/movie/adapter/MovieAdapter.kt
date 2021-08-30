079aab100d214d832247707aa60aa35b305b0476
KiD <trungnguyen96a4@gmail.com>
2021-08-30 12:11:31 +0700

    Add loading animation

--- app/src/main/java/gst/trainingcourse/movie/ui/fragment/category/movie/adapter/MovieAdapter.kt
+++ app/src/main/java/gst/trainingcourse/movie/ui/fragment/category/movie/adapter/MovieAdapter.kt
@@ -1,14 +1,20 @@
 package gst.trainingcourse.movie.ui.fragment.category.movie.adapter
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import gst.trainingcourse.movie.BuildConfig
 import gst.trainingcourse.movie.R
 import gst.trainingcourse.movie.data.model.MovieResponse
 import gst.trainingcourse.movie.databinding.ItemMovie2Binding
 import gst.trainingcourse.movie.ui.adapter.BaseAdapter
 
-class MovieAdapter (private val onItemClick: (MovieResponse.Movie, Int) -> Unit) :
+class MovieAdapter(private val onItemClick: (MovieResponse.Movie, Int) -> Unit) :
     BaseAdapter<MovieResponse.Movie, ItemMovie2Binding>(R.layout.item_movie2) {
 
     private val requestOptions by lazy {
@@ -30,8 +36,29 @@ class MovieAdapter (private val onItemClick: (MovieResponse.Movie, Int) -> Unit)
             Glide.with(root.context)
                 .load("${BuildConfig.BASE_IMAGE_URL}${getItem(position).posterPath}")
                 .apply(requestOptions)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        holder.binding.progressLoading.visibility = View.GONE
+                        return false
+                    }
+                })
                 .into(imageViewPoster)
-            textViewName.text=getItem(position).title
+            textViewName.text = getItem(position).title
         }
 
     }
--- app/src/main/java/gst/trainingcourse/movie/ui/fragment/home/adapter/MovieAdapter.kt
+++ app/src/main/java/gst/trainingcourse/movie/ui/fragment/home/adapter/MovieAdapter.kt
@@ -1,9 +1,15 @@
 package gst.trainingcourse.movie.ui.fragment.home.adapter
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import gst.trainingcourse.movie.BuildConfig
 import gst.trainingcourse.movie.R
 import gst.trainingcourse.movie.data.model.MovieResponse
@@ -33,6 +39,27 @@ class MovieAdapter(private val onItemClick: (MovieResponse.Movie, Int) -> Unit) 
             Glide.with(root.context)
                 .load("${BuildConfig.BASE_IMAGE_URL}${getItem(position).posterPath}")
                 .apply(requestOptions)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        holder.binding.progressLoading.visibility = View.GONE
+                        return false
+                    }
+                })
                 .into(imageView)
         }
     }
